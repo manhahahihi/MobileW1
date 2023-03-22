da7136f1229b4ec1e5e743c386147e21eb5e3757
manhahahihi <nmanh4138@gmail.com>
2023-03-22 16:10:26 +0700

    up

--- Ex2_week1/app/src/main/java/com/example/ex2_week1/MainActivity.java
+++ Ex2_week1/app/src/main/java/com/example/ex2_week1/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.ex2_week1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.content.Intent;
+import android.net.Uri;
+
 
 public class MainActivity extends AppCompatActivity {
 
